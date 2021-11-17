b712f17a0cda598bc4be22c80134007514843cf3
kadir12386 <kadir12386@gmail.com>
2021-11-17 17:14:36 +0300

    First Change

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+
+export default function App() {
+  const users = [
+    {
+      name: "Sangeetha",
+      pic: "https://helostatus.com/wp-content/uploads/2021/09/lovely-cute-girl-pic.jpg",
+    },
+    {
+      name: "Suba",
+      pic: "https://helostatus.com/wp-content/uploads/2021/08/best-profile-pictures-2021.jpg",
+    },
+  ];
+  return (
+    <div className="App">
+      {users.map((el) => (
+        <Msg name={el.name} pic={el.pic} />
+      ))}
+    </div>
+  );
+}
+
+function Msg({ name, pic }) {
+  return (
+    <div>
+      <img className="profile-pic" src={pic} />
+      <h1>{name}</h1>
+    </div>
+  );
+}
